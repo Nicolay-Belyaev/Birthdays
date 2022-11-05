4f49956a424a6c115fdb7cf05c9cfe5a94ae67c7
Nicolay-Belyaev <nickb2147@gmail.com>
2022-11-05 17:16:19 +0300

    + comments

--- main.py
+++ main.py
@@ -20,22 +20,25 @@ employees = {
 
 
 def gift_count(budget: int, birthdays: dict, month):
+    # соберем словарь из тех, у кого день рождения в этом месяце
     month_birthdays = {}
     for record in birthdays.items():
         if record[1].month == month:
             month_birthdays[record[0]] = record[1]
     birthday_boys_counter = len(month_birthdays)
+
+    # проверим, может быть в этом месяце дней рождения нет
     if birthday_boys_counter == 0:
         print('В этом месяце нет именинников.')
         return
 
-    sorted_month_birthdays_tuple = sorted(month_birthdays.items(), key=lambda x: x[1].day)
-    result_string = f'Именинники в месяце {birthday_boys_counter}: '
+    sorted_month_birthdays_tuple = sorted(month_birthdays.items(), key=lambda x: x[1].day)  # отсортируем по номер дня
+    result_string = f'Именинники в месяце {birthday_boys_counter}: '  # начнем собирать строку
     for birthday_boy in sorted_month_birthdays_tuple:
-        result_string += f'{birthday_boy[0]}({birthday_boy[1].strftime("%d.%m.%Y")}), '
-    gift = round(budget / birthday_boys_counter)
+        result_string += f'{birthday_boy[0]}({birthday_boy[1].strftime("%d.%m.%Y")}), '  # добавим имена и даты
+    gift = round(budget / birthday_boys_counter)  # посчитаем, сколько достанется каждому
 
-    result_string = result_string[0:-2] + '.' + f' При бюджете {budget} они получат по {gift} рублей.'
+    result_string = result_string[0:-2] + '.' + f' При бюджете {budget} они получат по {gift} рублей.'  # дособираем всё
     print(result_string)
 
 
